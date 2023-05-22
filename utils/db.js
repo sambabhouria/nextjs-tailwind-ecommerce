33395767ea5586d97b107f740dfebb7b5dd44d9d
Samba Diallo <sambadiallo@MacBook-Pro-de-Samba.local>
2023-05-22 21:01:20 +0200

    lecon 13 mongodb mongoose create db utils file create sample users

--- utils/db.js
+++ utils/db.js
@@ -30,12 +30,6 @@ async function disconnect() {
     }
   }
 }
-function convertDocToObj(doc) {
-  doc._id = doc._id.toString();
-  doc.createdAt = doc.createdAt.toString();
-  doc.updatedAt = doc.updatedAt.toString();
-  return doc;
-}
 
-const db = { connect, disconnect, convertDocToObj };
+const db = { connect, disconnect };
 export default db;
