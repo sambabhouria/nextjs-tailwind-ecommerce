53e363b525455d3be05f8940b85d27a6e0dcb706
Samba Diallo <sambadiallo@MacBook-Pro-de-Samba.local>
2023-05-23 13:09:52 +0200

    lecon_17 payment method : paypal stripe

--- utils/Store.js
+++ utils/Store.js
@@ -6,7 +6,7 @@ export const Store = createContext();
 const initialState = {
   cart: Cookies.get('cart')
     ? JSON.parse(Cookies.get('cart'))
-    : { cartItems: [], shippingAddress: {} },
+    : { cartItems: [], shippingAddress: {}, paymentMethod: '' },
 };
 
 function reducer(state, action) {
@@ -33,7 +33,6 @@ function reducer(state, action) {
     }
     case 'CART_RESET':
       return {
-        // return rest of state and reset cart
         ...state,
         cart: {
           cartItems: [],
@@ -50,6 +49,14 @@ function reducer(state, action) {
             ...state.cart.shippingAddress,
             ...action.payload,
           },
+        },
+      };
+    case 'SAVE_PAYMENT_METHOD':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          paymentMethod: action.payload,
         },
       };
     default:
