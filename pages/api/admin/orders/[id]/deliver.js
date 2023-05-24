5730675d240153aed485f422524e526588bdcf48
Samba Diallo <sambadiallo@MacBook-Pro-de-Samba.local>
2023-05-24 17:48:46 +0200

    add delivred bouton

---
+++ pages/api/admin/orders/[id]/deliver.js
@@ -0,0 +1,40 @@
+// import { getSession } from 'next-auth/react';
+import { authOptions } from 'pages/api/auth/[...nextauth]';
+import { getServerSession } from 'next-auth/next';
+import Order from '../../../../../models/Order';
+import db from '../../../../../utils/db';
+import User from '@/models/User';
+
+const handler = async (req, res) => {
+  // const session = await getSession({ req });
+  const session = await getServerSession(req, res, authOptions);
+  await db.connect();
+  const userFromDataBase = await User.findOne({ email: session.user.email });
+
+  const { isAdmin } = userFromDataBase;
+
+  // if (!session || (session && !session.user.isAdmin)) {
+  //   return res.status(401).send('Error: signin required');
+  // }
+
+  if (!session || (session && !isAdmin)) {
+    return res.status(401).send('Error: signin required');
+  }
+
+  const order = await Order.findById(req.query.id);
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+    const deliveredOrder = await order.save();
+    await db.disconnect();
+    res.send({
+      message: 'order delivered successfully',
+      order: deliveredOrder,
+    });
+  } else {
+    await db.disconnect();
+    res.status(404).send({ message: 'Error: order not found' });
+  }
+};
+
+export default handler;
