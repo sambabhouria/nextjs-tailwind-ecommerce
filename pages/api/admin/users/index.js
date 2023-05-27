96a2b9fd4177134c123050a381837ea88733d6a6
Samba Diallo <sambadiallo@MacBook-Pro-de-Samba.local>
2023-05-27 13:19:20 +0200

    upadate next to next 13

--- pages/api/admin/users/index.js
+++ pages/api/admin/users/index.js
@@ -1,12 +1,18 @@
-import { getSession } from 'next-auth/react'
+// import { getSession } from 'next-auth/react'
 import User from '../../../../models/User'
 import db from '../../../../utils/db'
+import { getToken } from 'next-auth/jwt'
 
 const handler = async (req, res) => {
-  const session = await getSession({ req })
-  if (!session || !session.user.isAdmin) {
+  const user = await getToken({ req })
+  if (!user || !user.isAdmin) {
     return res.status(401).send('admin signin required')
   }
+
+  // const session = await getSession({ req })
+  // if (!session || !session.user.isAdmin) {
+  //   return res.status(401).send('admin signin required')
+  // }
   await db.connect()
   const users = await User.find({})
   await db.disconnect()
